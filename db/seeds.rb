1c1db26f433edd0ac13baa28b5fe2a8a4fbac801
morwen80 <ribesnero80@gmail.com>
2018-12-25 20:11:02 +0000

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+pop = Genre.new("pop")
+k-pop = Genre.new("k-pop")
+
+b1a4 = Artist.new("B1A4", "The best boy-band of Korea")
+
+soloday = Song.new("Solo Day")
